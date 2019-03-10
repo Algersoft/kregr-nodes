60c9b5810ce7adc5c943a4c65b9a9fb9a324804a
Dr. Krieger <46250365+DocKrieger@users.noreply.github.com>
2019-03-10 01:23:15 +0000

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,5 @@
+const apiInterface = 'https://nodes.algersoft.org/'
+const nodes = [
+  { name: 'US-East-1', hostname: 'node-1.nyc.algersoft.org', port: 11766, region: 'US East' },
+  { name: 'SEED', hostname: 'seed.algersoft.org', port: 11766, region: 'US East' }
+]
